e16073bc07141a3dcf6aa8fa03f317aa58e71bc8
woodwardtw <twwoodward@me.com>
2021-09-01 16:01:24 -0400

    add room submit front-end

---
+++ loop-templates/content-room-submit.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * Partial template for content in page.php
+ *
+ * @package UnderStrap
+ */
+
+// Exit if accessed directly.
+defined( 'ABSPATH' ) || exit;
+?>
+
+<article <?php post_class(); ?> id="post-<?php the_ID(); ?>">
+
+	<header class="entry-header">
+
+		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+
+	</header><!-- .entry-header -->
+
+	<?php echo get_the_post_thumbnail( $post->ID, 'large' ); ?>
+
+	<div class="entry-content">
+
+		<?php the_content(); ?>
+		 <?php acf_form(array(
+        'post_id'       => 'new_post',
+        'new_post'      => array(
+            'post_type'     => 'room',
+            'post_status'   => 'publish'
+        ),
+        'submit_value'  => 'Create new room evaluation'
+    )); ?>
+
+		<?php
+		wp_link_pages(
+			array(
+				'before' => '<div class="page-links">' . __( 'Pages:', 'understrap' ),
+				'after'  => '</div>',
+			)
+		);
+		?>
+
+	</div><!-- .entry-content -->
+
+	<footer class="entry-footer">
+
+		<?php edit_post_link( __( 'Edit', 'understrap' ), '<span class="edit-link">', '</span>' ); ?>
+
+	</footer><!-- .entry-footer -->
+
+</article><!-- #post-## -->
