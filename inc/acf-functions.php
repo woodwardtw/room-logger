229c21355cf5c73141e066e54d5904d1068ad940
woodwardtw <twwoodward@me.com>
2021-09-01 15:43:09 -0400

    check added

--- inc/acf-functions.php
+++ inc/acf-functions.php
@@ -11,12 +11,17 @@ defined( 'ABSPATH' ) || exit;
 function dlinq_room_system(){
     global $post;
     $system_ids = get_field('system', $post->ID);
-    foreach ($system_ids as $key => $system) {
+    if($system_ids){
+         foreach ($system_ids as $key => $system) {
         $term = get_term( $system );
         $system_name = $term->name;
         $system_link = get_term_link($system);
         echo "<li><a href='{$system_link}'>{$system_name}</a></li>";
+        }
+    } else {
+        echo "<li>None listed</li>";        
     }
+   
 }
 
 function dlinq_room_system_details(){
@@ -30,33 +35,48 @@ function dlinq_room_system_details(){
 function dlinq_room_mics(){
     global $post;
     $mics = get_field('mic', $post->ID);
-    foreach ($mics as $key => $mic) {
+    if($mics){
+        foreach ($mics as $key => $mic) {
         $term = get_term( $mic );
         $mic_name = $term->name;
         $mic_link = get_term_link($mic);
         echo "<li><a href='{$mic_link}'>{$mic_name}</a></li>";
+        }
+    } else{
+        echo "<li>None listed</li>";
     }
+    
 }
 
 function dlinq_room_audio(){
     global $post;
     $speakers = get_field('speaker', $post->ID);
-    foreach ($speakers as $key => $speaker) {
+    if($speakers){
+        foreach ($speakers as $key => $speaker) {
         $term = get_term( $speaker );
         $speaker_name = $term->name;
         $speaker_link = get_term_link($speaker);
         echo "<li><a href='{$speaker_link}'>{$speaker_name}</a></li>";
+        }
+    } else {
+        echo "<li>None listed</li>";
     }
+    
 }
 
 function dlinq_room_hardware(){
     global $post;
     $hardwares = get_field('additional_hardware', $post->ID);
-     foreach ($hardwares as $key => $hardware) {
+    if($hardwares){
+        foreach ($hardwares as $key => $hardware) {
         $term = get_term( $hardware );
         $hardware_name = $term->name;
         $hardware_link = get_term_link($hardware);
         echo "<li><a href='{$hardware_link}'>{$hardware_name}</a></li>";
+        }
+    } else {
+        echo "<li>None listed</li>";
     }
+     
 }
 
